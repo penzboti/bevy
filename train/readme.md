03318464beb5ec3766e9ffae92b40ffcd60e8b26
penzboti <penzboti@gmail.com>
2025-07-22 12:54:38 +0200

    ldtk map loading and readme

---
+++ train/readme.md
@@ -0,0 +1,19 @@
+# info
+This is a pc recreation of the taken down mobile game "tracky train".
+Its gonna be 2d top down pixel art instead of orthographic voxels.
+The scoring is gonna be somewhat different aswell.
+The core gameplay doesn't change much tho.
+The maps are all custom.
+
+# credits
+Followed the bevy_ecs_ldtk book very closely (copied & unchanged code).
+
+# todo
+- [x] basic project start (movement, test level)
+- [ ] get movement buffering
+- [ ] load levels (multiple LdtkWorldBundles)
+- [ ] scoring
+- [ ] assets
+- [ ] levels
+- [ ] ui
+- [ ] menus
--- train/src/main.rs
+++ train/src/main.rs
@@ -1,39 +1,62 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+use std::collections::HashSet;
 
 fn main() {
     App::new()
         // init
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(LdtkPlugin)
+        .register_ldtk_entity::<PlayerBundle>("Player") // can i put this statement anywhere else?
+        .register_ldtk_int_cell::<WallBundle>(1) // and this one
+        // yes you can its called plugins brotha
         .add_systems(Startup, setup_canvas)
-        .add_systems(Startup, (setup_grid, setup_player).chain())
-        .add_systems(Update, (handle_keypress, game_tick).chain())
+        .add_systems(Startup, setup_level)
+        .add_systems(Update, cache_wall_locations)
+        .add_systems(
+            Update,
+            (handle_keypress, game_tick, translate_grid_coords_entities).chain(),
+        )
         .run();
 }
 
-const GRID_SIZE: f32 = 40.;
+const GRID_SIZE: i32 = 16;
 const SECONDS_PER_TICK: f32 = 1.;
 
-fn setup_canvas(
-    mut commands: Commands,
-) {
+fn setup_canvas(mut commands: Commands) {
     // camera
-    commands.spawn(Camera2d);
+    commands.spawn((
+        Camera2d,
+        Projection::Orthographic(OrthographicProjection {
+            scale: 0.5,
+            ..OrthographicProjection::default_2d()
+        }),
+        Transform::from_xyz(1280.0 / 4.0, 720.0 / 4.0, 0.0),
+    ));
+
     // background color
-    commands.insert_resource(ClearColor(Color::srgb(0.,0.,0.)));
+    commands.insert_resource(ClearColor(Color::srgb(0., 0., 0.)));
     // init tick system
-    commands.insert_resource(GameTickTimer(Timer::from_seconds(SECONDS_PER_TICK, TimerMode::Repeating)));
+    commands.insert_resource(GameTickTimer(Timer::from_seconds(
+        SECONDS_PER_TICK,
+        TimerMode::Repeating,
+    )));
     // init tracks and train
     commands.insert_resource(Train {
-        carriages: vec![Carriage { position: Vec2::ZERO }],
-        tracks: vec![]
+        carriages: vec![Carriage {
+            position: Vec2::ZERO,
+        }],
+        tracks: vec![],
     });
 }
 
-#[derive(Component)]
+#[derive(Default, Component)]
 struct Player {
-    pub position: Vec2,
     pub direction: Direction,
-    pub list_next_directions: Vec<Direction>
+    pub list_next_directions: Vec<Direction>,
 }
 
 #[derive(Component)]
@@ -49,127 +72,195 @@ struct Train {
 
 #[derive(Component)]
 struct Track {
-    pub position: Vec2,
-    pub direction: Direction
+    pub grid_coords: GridCoords,
+    pub direction: Direction,
 }
 
 #[derive(Component)]
 struct Tile {
-    pub position: Vec2,
+    pub grid_coords: GridCoords,
 }
 
-#[derive(Clone, PartialEq)]
+#[derive(Default, Clone, PartialEq)]
 enum Direction {
+    #[default]
     North,
     South,
     East,
-    West
+    West,
 }
 
 #[derive(Resource, Deref, DerefMut)]
 struct GameTickTimer(Timer);
 
-fn setup_player(
-    mut commands: Commands,
-) {
-    // player
-    commands.spawn((
-        // a one by one cube of color
-        Sprite::from_color(Color::srgb(1.,1.,1.), Vec2::ONE),
-        Transform {
-            // position
-            translation: Vec3::ZERO,
-            // size (multiplying the actual size)
-            scale: Vec2::new(GRID_SIZE,GRID_SIZE).extend(1.0),
-            // scale: Vec3::ONE,
-            ..default()
-        },
-        Player {
-            position: Vec2::ZERO,
-            direction: Direction::North,
-            list_next_directions: vec![],
-        },
-    ));    
+#[derive(Default, Bundle, LdtkEntity)]
+struct PlayerBundle {
+    player: Player,
+    #[sprite_sheet]
+    sprite_sheet: Sprite,
+    #[grid_coords]
+    grid_coords: GridCoords,
 }
 
-fn setup_grid (
-    mut commands: Commands,
-) {
-    for i in 0..=10 {
-        for j in 0..=10 {
-            let n1 = i as f32;
-            let n2 = j as f32;
-            let color = if (i+j) % 2 == 0 { Color::srgb(0.5, 0., 0.) } else { Color::srgb(0., 0., 0.5) };
-            commands.spawn((
-                Sprite::from_color(color, Vec2::ONE),
-                Transform {
-                    translation: Vec3::new(GRID_SIZE*n1, GRID_SIZE*n2, 0.),
-                    scale: Vec2::new(GRID_SIZE,GRID_SIZE).extend(1.),
-                    ..default()
-                },
-                Tile {
-                    position: Vec2 { x: n1, y: n2 }}
-            ));
-        }
+#[derive(Default, Component)]
+struct Wall;
+
+#[derive(Default, Bundle, LdtkIntCell)]
+struct WallBundle {
+    wall: Wall,
+}
+
+#[derive(Default, Resource)]
+struct LevelWalls {
+    wall_locations: HashSet<GridCoords>,
+    level_width: i32,
+    level_height: i32,
+}
+
+impl LevelWalls {
+    fn in_wall(&self, grid_coords: &GridCoords) -> bool {
+        grid_coords.x < 0
+            || grid_coords.y < 0
+            || grid_coords.x >= self.level_width
+            || grid_coords.y >= self.level_height
+            || self.wall_locations.contains(grid_coords)
     }
+}
+
+// #[derive(Default, Bundle, LdtkEntity)]
+// struct GoalBundle {
+//     #[sprite_sheet_bundle]
+//     sprite_sheet_bundle: SpriteSheetBundle,
+// }
+
+fn setup_level(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // load the entire project
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("train.ldtk").into(),
+        ..Default::default()
+    });
+    // select initial level
+    commands.insert_resource(LevelSelection::index(0));
+    // setup entities
+    // commands.regiset_ldtk_entity::<PlayerBundle>("Player");
+    commands.init_resource::<LevelWalls>();
 }
 
 // this is hard because of "koyote time"
 // i can hug a wall and follow its direction
 // i can pre-press the button and the game just uses that movement
-fn handle_keypress(
-    mut player_query: Query<&mut Player>,
-    keys: Res<ButtonInput<KeyCode>>,
-) {
+fn handle_keypress(mut player_query: Query<&mut Player>, keys: Res<ButtonInput<KeyCode>>) {
     if let Ok(mut player) = player_query.single_mut() {
         let dir = player.direction.clone();
-        if keys.just_pressed(KeyCode::KeyW){
-            if dir == Direction::North {player.list_next_directions = vec![];}
-            if dir != Direction::South { player.list_next_directions.push(Direction::North); }
+        if keys.just_pressed(KeyCode::KeyW) {
+            if dir == Direction::North {
+                player.list_next_directions = vec![];
+            }
+            if dir != Direction::South {
+                player.list_next_directions.push(Direction::North);
+            }
         }
-        if keys.just_pressed(KeyCode::KeyA){
-            if dir == Direction::West {player.list_next_directions = vec![];}
-            if dir != Direction::East { player.list_next_directions.push(Direction::West); }
+        if keys.just_pressed(KeyCode::KeyA) {
+            if dir == Direction::West {
+                player.list_next_directions = vec![];
+            }
+            if dir != Direction::East {
+                player.list_next_directions.push(Direction::West);
+            }
         }
-        if keys.just_pressed(KeyCode::KeyS){
-            if dir == Direction::South {player.list_next_directions = vec![];}
-            if dir != Direction::North { player.list_next_directions.push(Direction::South); }
+        if keys.just_pressed(KeyCode::KeyS) {
+            if dir == Direction::South {
+                player.list_next_directions = vec![];
+            }
+            if dir != Direction::North {
+                player.list_next_directions.push(Direction::South);
+            }
         }
-        if keys.just_pressed(KeyCode::KeyD){
-            if dir == Direction::East {player.list_next_directions = vec![];}
-            if dir != Direction::West { player.list_next_directions.push(Direction::East); }
+        if keys.just_pressed(KeyCode::KeyD) {
+            if dir == Direction::East {
+                player.list_next_directions = vec![];
+            }
+            if dir != Direction::West {
+                player.list_next_directions.push(Direction::East);
+            }
         }
     }
 }
 
 fn game_tick(
-    mut player_query: Query<(&mut Player, &mut Transform)>,
-    mut train: ResMut<Train>,
+    mut player_query: Query<(&mut Player, &mut GridCoords)>,
+    // mut train: ResMut<Train>,
     time: Res<Time>,
     mut timer: ResMut<GameTickTimer>,
+    level_walls: Res<LevelWalls>,
 ) {
-       timer.tick(time.delta());
+    timer.tick(time.delta());
 
     if timer.finished() {
-        if let Ok((mut player, mut transform)) = player_query.single_mut() {
-            player.direction = player.list_next_directions.pop().unwrap_or(&player.direction.clone()).clone();
-            if (true) {
-                // if a wall is present and the last action was to ride that, then don't clear
-                // for that i need the map (loaded and queried)
-                player.list_next_directions = vec![];
-            }
+        if let Ok((mut player, mut grid_coords)) = player_query.single_mut() {
+            player.direction = player
+                .list_next_directions
+                .pop()
+                .unwrap_or(player.direction.clone())
+                .clone();
+            // if a wall is present and the last action was to ride that, then don't clear
+            // for that i need the map (loaded and queried)
+            player.list_next_directions = vec![];
             let dir = player.direction.clone();
-            train.tracks.push(Track{
-                position: player.position.clone(),
-                direction: dir.clone(),
-            });
-            player.position += Vec2::from(match dir {
-                Direction::North => (0.,1.),
-                Direction::West => (-1.,0.),
-                Direction::South => (0.,-1.),
-                Direction::East => (1.,0.)
-            });
-            transform.translation = Vec3::new(player.position.x as f32 * GRID_SIZE,player.position.y as f32 * GRID_SIZE, 0.);
+            // train.tracks.push(Track {
+            //     grid_coords: player.grid_coords.clone(),
+            //     direction: dir.clone(),
+            // });
+            let destination = *grid_coords
+                + match dir {
+                    Direction::North => GridCoords::new(0, 1),
+                    Direction::West => GridCoords::new(-1, 0),
+                    Direction::South => GridCoords::new(0, -1),
+                    Direction::East => GridCoords::new(1, 0),
+                };
+            if !level_walls.in_wall(&destination) {
+                *grid_coords = destination;
+            }
         }
     }
+}
+
+fn translate_grid_coords_entities(
+    mut grid_coords_entities: Query<(&mut Transform, &GridCoords), Changed<GridCoords>>,
+) {
+    for (mut transform, grid_coords) in grid_coords_entities.iter_mut() {
+        transform.translation =
+            bevy_ecs_ldtk::utils::grid_coords_to_translation(*grid_coords, IVec2::splat(GRID_SIZE))
+                .extend(transform.translation.z);
+    }
+}
+
+fn cache_wall_locations(
+    mut level_walls: ResMut<LevelWalls>,
+    mut level_events: EventReader<LevelEvent>,
+    walls: Query<&GridCoords, With<Wall>>,
+    ldtk_project_entities: Query<&LdtkProjectHandle>,
+    ldtk_project_assets: Res<Assets<LdtkProject>>,
+) -> Result {
+    for level_event in level_events.read() {
+        if let LevelEvent::Spawned(level_iid) = level_event {
+            let ldtk_project = ldtk_project_assets
+                .get(ldtk_project_entities.single()?)
+                .expect("LdtkProject should be loaded when level is spawned");
+            let level = ldtk_project
+                .get_raw_level_by_iid(level_iid.get())
+                .expect("spawned level should exist in project");
+
+            let wall_locations = walls.iter().copied().collect();
+
+            let new_level_walls = LevelWalls {
+                wall_locations,
+                level_width: level.px_wid / GRID_SIZE,
+                level_height: level.px_hei / GRID_SIZE,
+            };
+
+            *level_walls = new_level_walls;
+        }
+    }
+    Ok(())
 }
